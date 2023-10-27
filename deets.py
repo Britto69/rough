3cdf5924b67c97b42b00a1ad73834da4e56fd203
Britto Fernandes <britto.fernandes@msds.christuniversity.in>
2023-10-27 21:20:41 +0530

    version01

---
+++ deets.py
@@ -0,0 +1,49 @@
+#define a classs expensetracker that stores expenses and income in dictionary implement the method to
+#store transactiom, view transaction and calculate total expense/income
+class expensetracker:
+    def __init__(self): #initializing empty dictionary
+        self.transactions={
+            "expenses":[],
+            "income":[]
+        }
+    def storetransaction(self,type,category,cost,desc,date):
+        transaction = {
+            "category":category,
+            "Cost":cost,
+            "Description":desc,
+            "Date":date
+        }
+        if type == "expenses":
+            self.transactions['expenses'].append(transaction)
+        else:
+            self.transactions['income'].append(transaction)
+        return True
+    
+    def view_transactions(self):
+        print("INCOME")
+        for abc in self.transactions['income']:
+            print(abc)
+        print("EXPENSES")
+        for abc in self.transactions['expenses']:
+            print(abc)
+
+    def total_incomeexpense(self):
+        print("Total Income")
+        total_income=0
+        for income in self.transactions['income']:
+            print(income["Cost"])
+        print(total_income)
+        print("Total Expense") 
+        total_expense=0
+        for expense in self.transactions['income']:
+            print(expense["Cost"])
+        print(total_expense)  
+
+mytransactions=expensetracker()
+mytransactions.storetransaction("income","Salary",10000,"Salary credited","01/09/2023")
+mytransactions.storetransaction("expense","Electricity Bill",4500,"MDHC","05/09/2023")
+mytransactions.storetransaction("income","Pocket money",10500,"from parents","07/09/2023")
+mytransactions.storetransaction("expense","Bday Treat",5000,"To friends","07/09/2023")
+
+mytransactions.view_transactions()
+mytransactions.total_incomeexpense()
